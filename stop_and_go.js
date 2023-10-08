cefaef714c1f124c1bcadda85457f72c62ba6272
awehero <147273991+awehero@users.noreply.github.com>
2023-10-07 21:59:05 -0400

    Update stop_and_go.js

--- stop_and_go.js
+++ stop_and_go.js
@@ -1,10 +1,11 @@
 var map = {
-    title: "Stop and Go",
+    title: "Test Map V8",
     song: "env2",
-    maker: "Awehero",
+    maker: "Ice dodo map maker",
     spawn: [0, 0.5, 0],
     init: function() {
         a.p([0, 0, -20], [0, 0, 0], [6, 0.5, 100], "1dc400", 0, 0, 0.6, false, false, false, false);
+        a.p([0, 0.25, -3], [0, -0.05, 0], [6, 0.3, 8], "1dc400", 0, 0, 0, false, false, false, false);
         a.p([0, -0.25, -20], [0, 0, 0], [100, 0.5, 100], "18a500", 0, 0, 0.6, false, true, false, false);
         a.e([0, 10, -20]);
     },
@@ -47,13 +48,14 @@ var map = {
     reset: function() {
         this.section_id = 0;
         a.re('P0', [0, 0, -20], [0, 0, 0], [6, 0.5, 100]);
-        a.re('P1', [0, -0.25, -20], [0, 0, 0], [100, 0.5, 100]);
+        a.re('P1', [0, 0.25, -3], [0, -0.05, 0], [6, 0.3, 8]);
+        a.re('P2', [0, -0.25, -20], [0, 0, 0], [100, 0.5, 100]);
         a.re('E0', [0, 10, -20], [0, 0, 0], [1, 1, 1]);
     },
     physics_update: function() {},
     render_update: function() {
         if (controls.space) {
-            speed = default_speed * 0.7;
+            speed = default_speed * 1;
         }
         if (controls.down) {
             speed = default_speed * 0;
