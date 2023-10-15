8593dd22f0b22753fee5ad240cbc7573187d9404
awehero <147273991+awehero@users.noreply.github.com>
2023-10-15 14:09:06 -0400

    Update stop_and_go.js

--- stop_and_go.js
+++ stop_and_go.js
@@ -4,17 +4,27 @@ var map = {
     maker: "Ice dodo map maker",
     spawn: [0, 0.5, 0],
     init: function() {
-        a.p([0, 0.25, -3], [0, -0.05, 0], [4, 0.3, 8], "1dc400", 0, 0, 0, false, false, false, false);
-        a.p([0, 0.01, -60], [0, 0, 0], [100, 0.5, 10], "a1a1a1", 0, 0, 0.6, false, true, false, false);
-        a.p([0, 0, -30], [0, 0, 0], [4, 0.5, 120], "1dc400", 0, 0, 0.6, false, false, false, false);
-        a.p([-6, 0, -80], [-0.45, 0, 0], [2, 0.5, 22], "1dc400", 0, 0, 0.6, false, false, false, false);
-        a.p([-10.5, 0, -78], [0, 0, 0], [0.5, 0.5, 16], "1dc400", 0, 0, 0.6, false, false, false, false);
-        a.p([0, 1, -60], [0, 0, 0], [4, 2, 2], "ff0000", 0, 0, 0.6, false, false, true, false);
-        a.p([0, -0.25, -80], [0, 0, 0], [1000, 0.5, 320], "18a500", 0, 0, 0.6, false, true, false, false);
-        a.e([0, 10, -20]);
+        a.p([0, -0.25, -79.99965], [0, 0, 0], [1000, 0.5, 320], "18a500", 0, 0, 0.6, false, true, false, false);
+        a.p([0, 0, -135.99958], [0, 0, 0], [2, 0.5, 10], "1dc400", 0, 0, 0.0, false, false, false, false);
+        a.p([0, 0.25, -2.99951], [0, -0.09, 0], [4, 0.3, 8], "1dc400", 0, 0, 0.0, false, false, false, false);
+        a.p([0, 0, -116], [0, 0, 0], [4, 0.5, 30], "1dc400", 0, 0, 0.6, true, false, false, false);
+        a.p([-8.99, 0, -81.49], [-0.79, 0, 0], [2, 0.5, 22], "1dc400", 0, 0, 0.6, false, false, false, false);
+        a.p([0, 0, -29.99986], [0, 0, 0], [4, 0.5, 120], "1dc400", 0, 0, 0.6, false, false, false, false);
+        a.p([-13.05, 0, -84.51], [-0.37, 0, 0], [1, 0.5, 22], "1dc400", 0, 0, 0.6, false, false, false, false);
+        a.p([-7.37, 0, -92.82], [0.58, 0, 0], [1.5, 0.5, 6], "1dc400", 0, 0, 0.6, false, false, false, false);
+        a.p([-5.91, 0, -93.77], [0, 0, 0], [1.5, 0.5, 6], "1dc400", 0, 0, 0.6, false, false, false, false);
+        a.p([-4.37, 0, -93.77], [0.51, 0, 0], [1.5, 0.5, 6], "1dc400", 0, 0, 0.6, false, false, false, false);
+        a.p([-2.19, 0, -96.46], [-0.17, 0, 0], [1.5, 0.5, 10], "1dc400", 0, 0, 0.6, false, false, false, false);
+        a.p([0, 0.01, -60], [0, 0, 0], [4, 0.5, 10], "a1a1a1", 0, 0, 0.6, false, true, false, false);
+        a.p([0, 0.01, -158.99993], [0, 0, 0], [4, 0.5, 4], "a1a1a1", 0, 0, 0.6, false, false, false, false);
+        a.p([0, 1, -59.99986], [0, 0, 0], [4, 2, 2], "ff0000", 999999.0, 0, 0.6, false, false, false, false);
+        a.p([0, 3, -109.99979], [0, 0, 0], [4, 6, 1], "ff0000,0.5", 999999.0, 0, 0.6, false, false, false, false);
+        a.p([0, 6, -112.99972], [0, 0, 0], [4, 6, 1], "ff0000,0.5", 999999.0, 0, 0.6, false, false, false, false);
+        a.e([0, 15.02, -195.28]);
     },
     post: function() {
-        a.u('P5');
+        a.u('P1');
+        a.u('P13');
         cc.set_monkey("scene.clearColor", new BABYLON.Color3.FromHexString("#ffffff"));
         cc.set_monkey("speed", default_speed * 0);
         cc.set_monkey("steer", 0.03);
@@ -25,37 +35,53 @@ var map = {
         let PZ = player.position.z;
         switch (this.section_id) {
         case 0:
-            if (PZ < -5) {
+             if (PZ < -5) {
                 a.msg_set("Press space to start!");
                 this.section_id += 1
             }
             break;
         case 1:
-            if (PZ < -15) {
-                a.msg_del();
-                this.section_id += 1
-            }
-            break;
-        case 2:
             if (PZ < -10) {
                 a.msg_set("Press the down arrow to stop!");
                 this.section_id += 1
             }
             break;
+        case 2:
+            if (PZ < -54.99979) {
+                this.section_id += 1
+            }
+            break;
         case 3:
-            if (PZ < -30) {
-                a.msg_del();
+            a.mov('P13', 'y', -0.1);
+            if (PZ < -64.99978999999999) {
                 this.section_id += 1
             }
             break;
         case 4:
-            if (PZ < -55) {
+            if (PZ < -100.99986) {
+                a.cam_cr(90.0);
+                a.jh(1.0);
+                steer = default_steer * 0.0;
+                rotation = 0.0 * Math.PI / 180;
                 this.section_id += 1
             }
             break;
         case 5:
-            a.mov('P5', 'y', -0.1);
-            if (PZ < -65) {
+            if (PZ < -120.99986) {
+                a.cam_cr(null);
+                a.jh(null);
+                steer = cc.get("steer", null);
+                this.section_id += 1
+            }
+            break;
+        case 6:
+            if (PZ < -132.5) {
+                this.section_id += 1
+            }
+            break;
+        case 7:
+            a.rot('P1', 'x', 10.0);
+            if (PZ < -141) {
                 this.section_id += 1
             }
             break;
@@ -63,14 +89,23 @@ var map = {
     },
     reset: function() {
         this.section_id = 0;
-        a.re('P0', [0, 0.25, -3], [0, -0.05, 0], [4, 0.3, 8]);
-        a.re('P1', [0, 0.01, -60], [0, 0, 0], [100, 0.5, 10]);
-        a.re('P2', [0, 0, -30], [0, 0, 0], [4, 0.5, 120]);
-        a.re('P3', [-6, 0, -80], [-0.45, 0, 0], [2, 0.5, 22]);
-        a.re('P4', [-10.5, 0, -78], [0, 0, 0], [0.5, 0.5, 16]);
-        a.re('P5', [0, 1, -60], [0, 0, 0], [4, 2, 2]);
-        a.re('P6', [0, -0.25, -80], [0, 0, 0], [1000, 0.5, 320]);
-        a.re('E0', [0, 10, -20], [0, 0, 0], [1, 1, 1]);
+        a.re('P0', [0, -0.25, -79.99965], [0, 0, 0], [1000, 0.5, 320]);
+        a.re('P1', [0, 0, -135.99958], [0, 0, 0], [2, 0.5, 10]);
+        a.re('P2', [0, 0.25, -2.99951], [0, -0.09, 0], [4, 0.3, 8]);
+        a.re('P3', [0, 0, -116], [0, 0, 0], [4, 0.5, 30]);
+        a.re('P4', [-8.99, 0, -81.49], [-0.79, 0, 0], [2, 0.5, 22]);
+        a.re('P5', [0, 0, -29.99986], [0, 0, 0], [4, 0.5, 120]);
+        a.re('P6', [-13.05, 0, -84.51], [-0.37, 0, 0], [1, 0.5, 22]);
+        a.re('P7', [-7.37, 0, -92.82], [0.58, 0, 0], [1.5, 0.5, 6]);
+        a.re('P8', [-5.91, 0, -93.77], [0, 0, 0], [1.5, 0.5, 6]);
+        a.re('P9', [-4.37, 0, -93.77], [0.51, 0, 0], [1.5, 0.5, 6]);
+        a.re('P10', [-2.19, 0, -96.46], [-0.17, 0, 0], [1.5, 0.5, 10]);
+        a.re('P11', [0, 0.01, -60], [0, 0, 0], [4, 0.5, 10]);
+        a.re('P12', [0, 0.01, -158.99993], [0, 0, 0], [4, 0.5, 4]);
+        a.re('P13', [0, 1, -59.99986], [0, 0, 0], [4, 2, 2]);
+        a.re('P14', [0, 3, -109.99979], [0, 0, 0], [4, 6, 1]);
+        a.re('P15', [0, 6, -112.99972], [0, 0, 0], [4, 6, 1]);
+        a.re('E0', [0, 15.02, -195.28], [0, 0, 0], [1, 1, 1]);
     },
     physics_update: function() {},
     render_update: function() {
