ca193c7a136c9d12f5a2531cbaf93efbdc1cbf99
awehero <147273991+awehero@users.noreply.github.com>
2023-11-04 23:27:32 -0400

    Update help.js

--- help.js
+++ help.js
@@ -1,11 +1,5 @@
-const fs = require('fs');
-
-(async () => {
-
-    const response =  await fetch('https://icedodo.onionfist.com/info?mapId=boat_bounce/');
-
-    const raw_html = await response.text();
-
-    fs.writeFileSync('boat-bounce.html', raw_html);
-
-})();
+$(document).ready(function () {
+    $.get("www.example.com/index.php", function(data) {
+        console.log(data)
+    }) ;
+});
