d97b80fdb2edb67f8148bd493ad41d887b2cc59e
awehero <147273991+awehero@users.noreply.github.com>
2024-04-09 18:03:34 -0400

    Create iceparty_help.js

---
+++ iceparty_help.js
@@ -0,0 +1,34 @@
+document.addEventListener('keydown', function(event) {
+    if (event.key === '1') {
+        window.tsTriggers.onWin();
+    }
+});
+document.addEventListener('keydown', function(event) {
+    if (event.key === '2') {
+        player.position.x = player.position.x + (-3 * Math.sin(rotation))
+        player.position.z = player.position.z + (-3 * Math.cos(rotation))
+    }
+});
+document.addEventListener('keydown', function(event) {
+   let grav = -9;
+   let gravctrl = 6;
+if (event.key === 'ArrowRight') {
+if (gravctrl == 4)
+    if (event.key === '3') {
+     grav = -3;
+     gravctrl = 4;   
+    }
+ if (event.key === '4') {
+     grav = 0;   
+    }
+ if (event.key === '5') {
+     grav = 2;   
+    }
+if (event.key === '6') {
+     grav = -9;   
+    }
+       scene.gravity = new BABYLON.Vector3(0, grav, 0);
+		gravity = scene.gravity;
+		scene.getPhysicsEngine().setGravity(scene.gravity);
+		player.applyGravity = true;
+});
