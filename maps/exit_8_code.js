d0ca03f4a8a5c026bda9f84c66a3d44d52bff5d3
awehero <147273991+awehero@users.noreply.github.com>
2025-09-30 18:56:49 -0400

    Update exit_8_code.js

--- maps/exit_8_code.js
+++ maps/exit_8_code.js
@@ -644,6 +644,7 @@ function test() {
                 if (alertCheck == 0) {
                     if (alpha == 5 && beta == 0) {
                         alert("Congrats, now that you've got the basics, feel free to play in a different mode or harder difficulty!");
+                        alertCheck = 1;
                     }
                     if (alpha == 10) {
                         if (beta == 0) {
