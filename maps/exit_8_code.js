f00688bb06aea52c0ac6c82c90a41d84e5cba299
awehero <147273991+awehero@users.noreply.github.com>
2025-09-30 09:08:00 -0400

    Update exit_8_code.js

--- maps/exit_8_code.js
+++ maps/exit_8_code.js
@@ -624,7 +624,7 @@ function test() {
             alert(string2);
         }
     }
-    if ((alpha == 9 || alpha > 19) || (alpha == 12 && beta == 1) || (alpha == 4 && beta == 0)) {
+    if ((alpha == 9 || alpha > 19) || (alpha == 12 && beta == 1) || (alpha == 5 && beta == 0)) {
         //objects.finish.position._y = 0;
         objects.visualfinish.position._y = 0;
         objects.fakefinish.position._y = 1.5;
